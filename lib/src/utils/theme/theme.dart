fc381c0187d75bdb3c7191fe352d9e13eced2e0c
HanZawNyein <HanZawNyein>
2023-04-18 23:39:57 +0630

    [develop] add theme style light and dark

---
+++ lib/src/utils/theme/theme.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:login_flutter_app/src/utils/theme/widget_themes/text_theme.dart';
+
+class TAppTheme {
+  static ThemeData lightTheme = ThemeData(
+    brightness: Brightness.light,
+    textTheme: TTextTheme.lightTextTheme,
+    elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(backgroundColor: Colors.red)),
+  );
+  static ThemeData darkTheme = ThemeData(
+      brightness: Brightness.dark, textTheme: TTextTheme.darkTextTheme);
+}
---
+++ lib/src/utils/theme/widget_themes/text_theme.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class TTextTheme {
+  static TextTheme lightTextTheme = TextTheme(
+    headlineLarge: GoogleFonts.montserrat(color: Colors.black87),
+    titleMedium: GoogleFonts.poppins(color: Colors.black87, fontSize: 24),
+    // bodyLarge:
+  );
+  static TextTheme darkTextTheme = TextTheme();
+}
