31ffe54491782e3f86c211aded7e712a72eb9ae2
HanZawNyein <HanZawNyein>
2023-04-19 16:02:06 +0630

    add theme in light ifx

--- lib/src/utils/theme/theme.dart
+++ lib/src/utils/theme/theme.dart
@@ -3,6 +3,8 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:login_flutter_app/src/utils/theme/widget_themes/text_theme.dart';
 
 class TAppTheme {
+  TAppTheme._();
+
   static ThemeData lightTheme = ThemeData(
     brightness: Brightness.light,
     textTheme: TTextTheme.lightTextTheme,
